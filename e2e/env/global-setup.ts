db74f950b34e834cd4903ce3e24d40af694e3127
csikb <22349790+csikb@users.noreply.github.com>
2021-12-26 09:10:55 +0100

    change wait strategy

--- e2e/env/global-setup.ts
+++ e2e/env/global-setup.ts
@@ -1,10 +1,13 @@
 import type { FullConfig } from '@playwright/test';
-import { DockerComposeEnvironment } from 'testcontainers';
+import { DockerComposeEnvironment, Wait } from 'testcontainers';
 
 async function globalSetup(config: FullConfig) {
   const composeEnvironment = new DockerComposeEnvironment(
     './',
     'docker-compose.yaml'
+  ).withWaitStrategy(
+    'frontend_1',
+    Wait.forLogMessage('Node Express server listening on')
   );
   const startedDockerComposeEnvironment = await composeEnvironment.up();
   const mappedPort = startedDockerComposeEnvironment
