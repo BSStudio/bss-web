7c872a3470d549642d2e0ba3ec3b1a9f9f9b388c
csikb <22349790+csikb@users.noreply.github.com>
2021-12-27 09:13:56 +0100

    add more test case

--- e2e/tests/example.spec.ts
+++ e2e/tests/example.spec.ts
@@ -1,6 +1,20 @@
 import { test, expect } from '@playwright/test';
 
-test('basic test', async ({ page, baseURL }) => {
-  await page.goto('/');
-  expect(await page.screenshot()).toMatchSnapshot('home.png');
+test.describe('test', () => {
+  test('basic test', async ({ page, baseURL }) => {
+    await page.goto('/');
+    expect(await page.screenshot()).toMatchSnapshot('home.png');
+  });
+  test('simple test', async ({ page }) => {
+    await page.goto('https://wikipedia.org');
+
+    await page.click('input[name="search"]');
+    // Fill input[name="search"]
+    await page.fill('input[name="search"]', 'angular js');
+    // Click a:has-text("AngularJSOpen source web application framework")
+    await page.click(
+      'a:has-text("AngularJSOpen source web application framework")'
+    );
+    await expect(page).toHaveURL('https://en.wikipedia.org/wiki/AngularJS');
+  });
 });
